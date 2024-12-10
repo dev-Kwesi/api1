ecd491a1825f176d340be6c5707bd3f592ee099d
Kwesi <kwesi@webmagnat.ro>
2024-12-10 16:39:49 +0000

    login-feature

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -49,6 +50,49 @@ class AuthController extends Controller
             'user'=>$user,
 
         ],200);
+
+    }
+
+    //login function
+    public function login(Request $request){
+
+        $validated= Validator::make($request->all(),[
+
+            'email'=>'required|string|email|max:255',
+            'password'=>'required|string|min:6'
+
+        ]);
+
+        if($validated->fails()){
+            return response()->json($validated->errors(),422);
+        }
+
+        $credentials = [
+            'email'=>$request->email,
+            'password'=>$request->password
+        ];
+
+        try {
+
+            if(!auth()->attempt($credentials)){
+                return response()->json(['error'=>'Invalide credetials'],403);
+            }
+
+            $user = User::where('email',$request->email)->firstorFail();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            
+        return response()->json([
+            'access_token'=>$token,
+            'user'=>$user,
+
+        ],200);
+            //code...
+        } catch (\Throwable $th) {
+            
+            return response()->json(['error'=>$th->getMessage()],403);
+        }
 
     }
 }
